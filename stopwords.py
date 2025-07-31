27a2413b0257fd31684284dcf994bba3b4fc04a8
laduarte1 <leonardoduarte1975@gmail.com>
2025-07-30 20:45:08 -0500

    se exploro nltk y las stopwords

---
+++ stopwords.py
@@ -0,0 +1,18 @@
+# importar la libreria nltk
+import nltk
+# desde nltk descargar el paquete stopwords
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+lista_stopwords = stopwords.words('spanish')
+#imprimir las stopwords
+print(lista_stopwords)
+
+
+touch stopwords_english.py
+
+
+
+
+
+
+
