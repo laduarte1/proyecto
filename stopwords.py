246ced59913c18c3e79a6b42e21188267690980c
laduarte1 <leonardoduarte1975@gmail.com>
2025-08-04 20:17:30 -0500

    Practicando con la IA de cursor

--- stopwords.py
+++ stopwords.py
@@ -8,7 +8,30 @@ lista_stopwords = stopwords.words('spanish')
 print(lista_stopwords)
 
 
-touch stopwords_english.py
+# Importar Libreria nltk
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+# definir la ruta donde se encuentra los datos descargados de nltk
+nltk.data.path.append('C:/Users/Leonardo_Duarte/AppData/Roaming/nltk_data')
+
+# Importar la librería NLTK 
+import nltk 
+
+# Desde NLTK, importamos las stopwords que son las palabras comunes en un idioma
+from nltk.corpus import stopwords
+
+# Descargamos la lista de palabras vacías stopwords sino está disponible en nuestra computadora
+nltk.download('stopwords')
+
+# Guardar en una variable la lista de stopwords en español
+lista_de_stopwords = stopwords.words('spanish')
+
+# Imprimir la lista de stopwords
+print(lista_de_stopwords)
+
 
 
 
